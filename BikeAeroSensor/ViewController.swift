bad2c4485eac03db15dd8fdd80ab246af0e88924
范祎楠 <>
2021-08-16 23:14:55 +0800

    init

---
+++ BikeAeroSensor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BikeAeroSensor
+//
+//  Created by 范祎楠 on 2021/8/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
